660f328d02bda6bdc3f1fc6162183df0b050b5d2
Manu Uzkudun <manuuzkudun@hotmail.com>
2019-01-26 12:02:31 +0100

    basic data added

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const basicData = require('./lib/basic_data.js');
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(
+    { 
+      "planets": basicData.planets,
+      "elements": basicData.elements,
+      "signs": basicData.signs.map(function(signData){
+        return {
+          "name": signData[0],
+          "element": basicData.elements[signData[1]]
+        }
+      })
+    }
+  ));
 });
 
 app.listen(3000, function () {
